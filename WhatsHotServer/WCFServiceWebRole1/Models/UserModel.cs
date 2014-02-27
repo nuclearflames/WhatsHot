0487f5d6fed490aaa7cc11399b9a8d08354be077
atwseo <ddevereux@outlook.com>
2014-02-27 16:33:24 +0000

    Added user model and db context
    Also corrected namespaces from default

---
+++ WhatsHotServer/WCFServiceWebRole1/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatsHotWCF.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string HashedPassword { get; set; }
+        public string DefaultPostcode { get; set; }
+    }
+}
