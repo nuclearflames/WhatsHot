c88d787248bcf7028865a231b797de16107dbf2a
jh1881 <jh1881@hotmail.com>
2014-02-27 18:39:33 +0000

    Models

---
+++ WhatsHotService/WCFServiceWebRole1/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceWebRole1.Models
+{
+    public class Location
+    {
+        public int ID;
+        public int UserId;
+        public string Lat;
+        public string Long;
+        public DateTime TimeAdded;
+    }
+}
