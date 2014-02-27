fa44f0efa531ec1f5543d8cd194c1c16f7be1b50
jh1881 <jh1881@hotmail.com>
2014-02-27 18:21:10 +0000

    Server stuff

--- WhatsHotService/WCFServiceWebRole1/IService1.cs
+++ WhatsHotService/WCFServiceWebRole1/IService1.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WCFServiceWebRole1.Models;
 
 namespace WCFServiceWebRole1
 {
@@ -17,9 +18,33 @@ namespace WCFServiceWebRole1
         [WebGet(UriTemplate = "GetData/{value}", ResponseFormat=WebMessageFormat.Json)]
         string GetData(string value);
 
-        
-    }
+        [OperationContract]
+        [WebInvoke(UriTemplate = "SetUserProfile/{token}/{defaultlocation}", ResponseFormat = WebMessageFormat.Json, Method="POST")]
+        void SetUserProfile(string token, string defaultlocation);
 
 
-    
+        [OperationContract]
+        [WebGet(UriTemplate = "GetUserProfile/{token}", ResponseFormat = WebMessageFormat.Json)]
+        string GetUserProfile(string token);
+
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Authenticate/{user}/{password}/{method}", ResponseFormat = WebMessageFormat.Json)]
+        string Authenticate(string user, string password, string method);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Register/{user}/{password}/{defaultlocation}", ResponseFormat = WebMessageFormat.Json, Method = "POST")]
+        string Register(string user, string password, string defaultlocation);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "PostDestination/{token}/{lat}/{long}", ResponseFormat = WebMessageFormat.Json, Method = "POST")]
+        void PostDestination(string token, string lat, string @long);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetHeatmapData/{token}/{lat}/{long}", ResponseFormat = WebMessageFormat.Json)]
+        HeatmapData[] GetHeatmapData(string token, string lat, string @long);
+
+    }    
 }
---
+++ WhatsHotService/WCFServiceWebRole1/Strings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceWebRole1
+{
+    internal static class Strings
+    {
+        public static string SqlConnection = "Server=tcp:xz2ynxwroq.database.windows.net,1433;Database=whatshot;User ID=whatshot@xz2ynxwroq;Password=qwerty1!;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+    }
+}
