060769a13f9d072cd197b8b66eedbf3a55888d19
atwseo <ddevereux@outlook.com>
2014-02-27 17:49:38 +0000

    Partial service classes added. Db context created. users model

---
+++ WhatsHotService/WCFServiceWebRole1/Contexts/WhatsHotContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WCFServiceWebRole1.Models;
+
+namespace WCFServiceWebRole1.Contexts
+{
+    public class WhatsHotContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ WhatsHotService/WCFServiceWebRole1/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceWebRole1.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string HashedPassword { get; set; }
+
+        public string DefaultLocation { get; set; }
+    }
+}
